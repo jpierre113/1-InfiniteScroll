abdd34fcf88c50a141d4d499562157b495f76409
Janna Pierre <gojanna@aol.com>
2021-06-22 19:06:36 -0400

    Installed and implemented Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import data from './nyc_ttp_pins.json'
 import React, { useState, useEffect, useRef } from 'react';
+import Card from 'react-bootstrap/Card';
 
 function App() {
   const perPage = 10;
@@ -45,10 +46,15 @@ function App() {
       <h1>Pinterest</h1>
       <div className="post-list">{
         pinsList.list.map((pin, index) => {
-          return (<div key={index} className="pins">
-            <img src={pin.images["236x"].url}/>
-            <p> {pin.description} </p>
-          </div>)
+          <div key={index} className="pins"></div>
+          return (
+            <Card>
+              <Card.Body>
+                <Card.Img src={pin.images["236x"].url}/>
+                <Card.Text> {pin.description} </Card.Text>
+              </Card.Body>
+          </Card>
+          )
         })
       }
         <div className="loading" ref={loader}></div>
