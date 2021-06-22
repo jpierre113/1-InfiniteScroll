53529c0279f4e88fe62a9abd123150f306c9479a
Janna Pierre <gojanna@aol.com>
2021-06-21 20:26:54 -0400

    Pins ID is able to render on page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import data from './nyc_ttp_pins.json'
 
 function App() {
+  const pinsData = [...data]
   return (
     <div className="App">
       <h1>Test</h1>
+      {pinsData.map((pin, index) => {
+        return(
+         <p>{pin.board.id}</p>
+        )   
+    })
+  }
     </div>
   );
 }
