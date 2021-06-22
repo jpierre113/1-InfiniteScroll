be8f8f3977233366e59a61717ee8b15bc770ac1f
Janna Pierre <gojanna@aol.com>
2021-06-21 20:42:02 -0400

    Implemented hook for pinsData

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import data from './nyc_ttp_pins.json'
+import React, { useState } from 'react';
 
 function App() {
-  const pinsData = [...data]
+  const [pinsData, setPins] = useState([...data]);
   return (
     <div className="App">
-      <h1>Test</h1>
+      <h1>Pinterest</h1>
       {pinsData.map((pin, index) => {
         return(
          <p>{pin.board.id}</p>
